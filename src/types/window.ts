07cc70d33a15e60757dfe6ac4c2e63f0c3a972e2
wankong <1549072621@qq.com>
2024-11-15 21:51:36 +0800

    feat:增加AI类型标注

---
+++ src/types/window.ts
@@ -0,0 +1,212 @@
+// base
+interface AIBaseOptions {
+    signal?: AbortSignal;
+}
+
+type AICapabilityAvailability = "readily" | "after_download" | "no"
+
+type AILanguageModelPromptRole = "user" | 'assistant'
+type AILanguageModelInitialPromptRole = "system" | AILanguageModelPromptRole;
+
+interface AICreateMonitor {
+    addEventListener(type: "downloadprogress", listener: (ev: { loaded: number; total: number }) => void, options?: boolean | AddEventListenerOptions): void;
+    removeEventListener(type: "downloadprogress", listener: (ev: { loaded: number; total: number }) => void, options?: boolean | EventListenerOptions): void;
+    // 可能在将来添加更多内容
+}
+type AICreateMonitorCallback = (monitor: AICreateMonitor) => void;
+
+
+// AIWriter
+// see more : https://github.com/WICG/writing-assistance-apis?tab=readme-ov-file#summarizer-api
+interface AIWriterFactory {
+    create(options?: AIWriterCreateOptions): Promise<AIWriter>;
+    capabilities(): Promise<AIWriterCapabilities>;
+}
+
+interface AIWriter {
+    write(writingTask: string, options?: AIWriterWriteOptions): Promise<string>;
+    writeStreaming(writingTask: string, options?: AIWriterWriteOptions): ReadableStream;
+
+    readonly sharedContext: string;
+    readonly tone: AIWriterTone;
+    readonly format: AIWriterFormat;
+    readonly length: AIWriterLength;
+
+    destroy(): void;
+}
+
+interface AIWriterCapabilities {
+    readonly available: AICapabilityAvailability;
+
+    createOptionsAvailable(options: AIWriterCreateCoreOptions): AICapabilityAvailability;
+    languageAvailable(languageTag: string): AICapabilityAvailability;
+}
+
+interface AIWriterCreateCoreOptions {
+    tone?: AIWriterTone;
+    format?: AIWriterFormat;
+    length?: AIWriterLength;
+}
+
+interface AIWriterCreateOptions extends AIWriterCreateCoreOptions, AIBaseOptions {
+    monitor?: AICreateMonitorCallback;
+    sharedContext?: string;
+}
+
+interface AIWriterWriteOptions extends AIBaseOptions {
+    context?: string;
+}
+
+type AIWriterTone = "formal" | "neutral" | "casual";
+type AIWriterFormat = "plain-text" | "markdown";
+type AIWriterLength = "short" | "medium" | "long";
+
+
+// summarizer 
+
+interface AISummarizerFactory {
+    create(options?: Partial<AISummarizerCreateOptions>): Promise<AISummarizer>;
+    capabilities(): Promise<AISummarizerCapabilities>;
+}
+
+interface AISummarizer {
+    summarize(input: string, options?: AISummarizerSummarizeOptions): Promise<string>;
+    summarizeStreaming(input: string, options?: AISummarizerSummarizeOptions): ReadableStream;
+
+    readonly sharedContext: string;
+    readonly type: AISummarizerType;
+    readonly format: AISummarizerFormat;
+    readonly length: AISummarizerLength;
+
+    destroy(): void;
+}
+
+interface AISummarizerCapabilities {
+    readonly available: AICapabilityAvailability;
+
+    createOptionsAvailable(options: AISummarizerCreateCoreOptions): AICapabilityAvailability;
+    languageAvailable(languageTag: string): AICapabilityAvailability;
+}
+
+interface AISummarizerCreateCoreOptions {
+    type?: AISummarizerType;
+    format?: AISummarizerFormat;
+    length?: AISummarizerLength;
+}
+
+interface AISummarizerCreateOptions extends AISummarizerCreateCoreOptions, AIBaseOptions {
+    monitor?: AICreateMonitorCallback;
+    sharedContext?: string;
+}
+
+interface AISummarizerSummarizeOptions extends AIBaseOptions {
+    context?: string;
+}
+
+type AISummarizerType = "tl;dr" | "key-points" | "teaser" | "headline";
+type AISummarizerFormat = "plain-text" | "markdown";
+type AISummarizerLength = "short" | "medium" | "long";
+
+
+// rewriter
+interface AIRewriterFactory {
+    create(options?: AIRewriterCreateOptions): Promise<AIRewriter>;
+    capabilities(): Promise<AIRewriterCapabilities>;
+}
+
+interface AIRewriter {
+    rewrite(input: string, options?: AIRewriterRewriteOptions): Promise<string>;
+    rewriteStreaming(input: string, options?: AIRewriterRewriteOptions): ReadableStream;
+
+    readonly sharedContext: string;
+    readonly tone: AIRewriterTone;
+    readonly format: AIRewriterFormat;
+    readonly length: AIRewriterLength;
+
+    destroy(): void;
+}
+
+interface AIRewriterCapabilities {
+    readonly available: AICapabilityAvailability;
+
+    createOptionsAvailable(options: AIRewriterCreateCoreOptions): AICapabilityAvailability;
+    languageAvailable(languageTag: string): AICapabilityAvailability;
+}
+
+interface AIRewriterCreateCoreOptions {
+    tone?: AIRewriterTone;
+    format?: AIRewriterFormat;
+    length?: AIRewriterLength;
+}
+
+interface AIRewriterCreateOptions extends AIRewriterCreateCoreOptions, AIBaseOptions {
+    monitor?: AICreateMonitorCallback;
+    sharedContext?: string;
+}
+
+interface AIRewriterRewriteOptions extends AIBaseOptions {
+    context?: string;
+}
+
+type AIRewriterTone = "as-is" | "more-formal" | "more-casual";
+type AIRewriterFormat = "as-is" | "plain-text" | "markdown";
+type AIRewriterLength = "as-is" | "shorter" | "longer";
+
+// AIAssistant
+// see more :https://github.com/explainers-by-googlers/prompt-api/
+
+interface AIAssistantPromptOptions extends AIBaseOptions {
+}
+
+interface AIAssistant {
+    prompt(input: string, options?: AIAssistantPromptOptions): Promise<string>;
+    promptStreaming(input: string, options?: AIAssistantPromptOptions): ReadableStream;
+
+    readonly sharedContext: string;
+    readonly temperature: number;
+    readonly topK: number;
+    readonly maxTokens: number;
+    readonly tokensSoFar: number;
+    readonly tokensLeft: number;
+
+    destroy(): void;
+
+    clone(): Promise<AIAssistant>;
+
+}
+
+interface AIAssistantCreateOptions extends AIBaseOptions {
+    monitor?: AICreateMonitorCallback;
+    sharedContext?: string;
+    systemPrompt?: string
+    initialPrompts?: {
+        role: AILanguageModelInitialPromptRole,
+        content: string
+    }[]
+}
+
+interface AIAssistantCapabilities {
+    readonly available: AICapabilityAvailability;
+    readonly defaultTemperature: number;
+    // Always null if available === "no"
+    readonly defaultTopK: number;
+    readonly maxTopK: number;
+}
+
+
+interface AIAssistantFactory {
+    create(options?: AIAssistantCreateOptions): Promise<AIAssistant>;
+    capabilities(): Promise<AIAssistantCapabilities>;
+}
+
+declare global {
+
+    interface Window {
+        ai: {
+            readonly assistant: AIAssistantFactory;
+            readonly writer: AIWriterFactory;
+            readonly rewriter: AIRewriterFactory;
+            readonly summarizer: AISummarizerFactory;
+        }
+    }
+}
